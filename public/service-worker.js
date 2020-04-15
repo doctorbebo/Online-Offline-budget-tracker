bdb57bc29d819e5ab38243779b3eacb002925a4d
doctorbebo <byronrb92@gmail.com>
2020-04-14 21:21:41 -0700

    service-worker.js activate and install working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,43 @@
-console.log("Hello from service worker");
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+]
+
+const CACHE_NAME = "static-cache-v3";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener('install',(event)=>
+{
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache)=>
+        {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+              console.log(key);
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
