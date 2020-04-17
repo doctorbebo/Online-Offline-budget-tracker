d5c296f8f4457591ad904c8c0198d8902b93af73
doctorbebo <byronrb92@gmail.com>
2020-04-16 20:41:44 -0700

    update to server js

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var mongoose = require("mongoose");
+var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/budget";
+const options = {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(MONGODB_URI,options)
 
 // routes
 app.use(require("./routes/api.js"));
